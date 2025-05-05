65afab03b9813864c88dd2081396ac92007a37f2
Vinicius Duzanski <viniciusduzanski@gmail.com>
2025-05-05 15:28:45 -0300

    feat: exercises resolved

---
+++ aula02/conceitos.py
@@ -0,0 +1,59 @@
+"""
+Tipos de dados:
+Inteiros (int): Representam números inteiros.
+Ponto Flutuante (float): Representam números reais.
+Strings (str): Representam sequências de caracteres.
+Booleanos (bool): Representam valores verdadeiros (True) ou falsos (False).
+
+1. Inteiros (int)
+Métodos e operações:
++ (adição)
+- (subtração)
+* (multiplicação)
+// (divisão inteira)
+% (módulo - resto da divisão)
+
+2. Números de Ponto Flutuante (float)
+Métodos e operações:
++ (adição)
+- (subtração)
+* (multiplicação)
+/ (divisão)
+** (potenciação)
+
+3. Strings (str)
+Métodos e operações:
+.upper() (converte para maiúsculas)
+.lower() (converte para minúsculas)
+.strip() (remove espaços em branco no início e no final)
+.split(sep) (divide a string em uma lista, utilizando sep como delimitador)
++ (concatenação de strings)
+
+4. Booleanos (bool)
+Operações lógicas:
+and (E lógico)
+or (OU lógico)
+not (NÃO lógico)
+== (igualdade)
+!= (diferença)
+"""
+
+nome_aluno = "Vinícius"
+
+# Verificando o tipo da variável
+print(type(nome_aluno))
+print(type("Vinícius"))
+
+# Converter para maiúsculas
+print(nome_aluno.upper())
+
+# Converter para minúsculas
+print(nome_aluno.lower())
+
+# Remover os espaços em branco no começo e no final da string
+nome_com_espacos = "   Teste com espaços   "
+print(nome_aluno.strip())
+
+# Dividir a string com um delimitador, vai criar uma lista com o resultado
+nome_delimitador = "viniciusduzanski@gmail.com"
+print(nome_delimitador.split("@"))
