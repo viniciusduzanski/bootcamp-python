4018f4bcd03526abe90b78a048e51641f0e749f7
Vinicius Duzanski <viniciusduzanski@gmail.com>
2025-05-09 14:46:07 -0300

    feat: class 3

---
+++ aula03/main.py
@@ -0,0 +1,21 @@
+# Estruturas de controle de fluxo
+# if / elif / else
+
+x = 2
+
+if x < 0:
+    x = 0
+    print("Negative changed to zero")
+    print("Negative changed to zero")
+elif x == 0:
+    print("Zero")
+elif x == 1:
+    print("Single")
+else:
+    print("More")
+
+for i in range(6):
+    print(i)
+
+for i in range(1, 6):
+    print(i)
